0e968d773e120e39c3664e46edffff6be42fd72f
Patryk D. Cichy <patryk.cichy@causeway.com>
2026-08-06 13:32:07 +0200

    escape double quotes (#2)
    revert commits break the JSON used as a slack API payload

--- tests/test_main.py
+++ tests/test_main.py
@@ -12,3 +12,8 @@ class Test(TestCase):
 
         expected = '''\n\n*What's Changed*\n*Tech changes*\n• inherit secrets by @PatTheSilent in https://github.com/letsbuilders/release-test/pull/4\n\n\n*Full Changelog*: https://github.com/letsbuilders/release-test/compare/v2026.3.12-1...v2026.3.12-2'''
         self.assertEqual(convert(test_text), expected)
+
+    def test_escape_double_quotes(self):
+        test_text = '''Revert "XXXX-9999 - Sort projects by IsActive flag" by @xxx'''
+        expected = r'''Revert \"XXXX-9999 - Sort projects by IsActive flag\" by @xxx'''
+        self.assertEqual(convert(test_text), expected)
